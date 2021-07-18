859ac8ac976f54e7f2771d8bb56178c54bcbecc5
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-18 00:59:11 -0400

    skeleton for product screen

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(imageUrl),
+    );
+  }
+}
