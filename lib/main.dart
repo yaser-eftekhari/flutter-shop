4aacb59baf701d8b01443a1bcc8ca5c778d3020a
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-18 21:34:09 -0400

    skeleton for the provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/products_overview_screen.dart';
+import 'screens/product_detail_screen.dart';
+
+import 'providers/products_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +14,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: "Lato",
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => ProductsProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: "Lato",
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+
+import 'product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.5,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) => ProductItem(
+        id: products[i].id,
+        title: products[i].title,
+        imageUrl: products[i].imageUrl,
+      ),
+    );
+  }
+}
