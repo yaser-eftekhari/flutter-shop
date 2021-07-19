9e8985f120399dd15b57b31d505b59e1554e4b9d
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-18 22:58:33 -0400

    added provider to product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -4,13 +4,15 @@ import 'package:provider/provider.dart';
 import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   static const routeName = "/product-detail";
 
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-    final productsData = Provider.of<ProductsProvider>(context);
+    final productsData = Provider.of<ProductsProvider>(
+      context,
+      listen: false,
+    );
 
     final product = productsData.findById(productId);
 
